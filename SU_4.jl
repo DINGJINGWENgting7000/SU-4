950e7a43e791837aa6d2eba49e3a817349c18f9b
WiseLu <zl991117@gmail.com>
2025-03-31 10:15:37 +0800

    DMRG_in_2D

--- SU_4.jl
+++ SU_4.jl
@@ -1,14 +1,20 @@
 using ITensors, ITensorMPS
 
-global Dim_site = 6  ## Define the dimension of Hilbert space in a site.
-global N = 4  ## Define the number of sites in the chain
+Dim_site = 6  ## Define the dimension of Hilbert space in a site.
+Lx = 4
+Ly = 3
+N = Lx*Ly  ## Define the number of sites in the chain
+n_in_cell = 1 ## Define the number of sublattices in a cell for triangular lattice
 
-function ITensors.space(::SiteType"IdxSO6")
+
+function ITensors.space(::SiteType"IdxSU4_6d")
     return Dim_site
 end
 
+
+
 ## Define the corresponding Operator of other generators. There should be 15 in total.
-function ITensors.op(::OpName"H1", ::SiteType"IdxSO6", s::Index)
+function ITensors.op(::OpName"H1", ::SiteType"IdxSU4_6d", s::Index)
   Op = emptyITensor(ComplexF64, s, s')
   Op[s'=>2, s=>2] = 1/2.0+0im
   Op[s'=>3, s=>3] = 1/2.0+0im
@@ -17,7 +23,7 @@ function ITensors.op(::OpName"H1", ::SiteType"IdxSO6", s::Index)
   return Op
 end
 
-function ITensors.op(::OpName"H2", ::SiteType"IdxSO6", s::Index)
+function ITensors.op(::OpName"H2", ::SiteType"IdxSU4_6d", s::Index)
   Op = emptyITensor(ComplexF64, s, s')
   Op[s'=>1, s=>1] = 2/3.0+0im
   Op[s'=>2, s=>2] = -1/3.0+0im
@@ -28,7 +34,7 @@ function ITensors.op(::OpName"H2", ::SiteType"IdxSO6", s::Index)
   return Op
 end
 
-function ITensors.op(::OpName"H3", ::SiteType"IdxSO6", s::Index)
+function ITensors.op(::OpName"H3", ::SiteType"IdxSU4_6d", s::Index)
   Op = emptyITensor(ComplexF64, s, s')
   Op[s'=>1, s=>1] = 1.0+0im
   Op[s'=>2, s=>2] = 1.0+0im
@@ -39,84 +45,84 @@ function ITensors.op(::OpName"H3", ::SiteType"IdxSO6", s::Index)
   return Op
 end
 
-function ITensors.op(::OpName"Ep1", ::SiteType"IdxSO6", s::Index)
+function ITensors.op(::OpName"Ep1", ::SiteType"IdxSU4_6d", s::Index)
   Op = emptyITensor(ComplexF64, s, s')
   Op[s'=>2, s=>4] = 1.0+0im
   Op[s'=>3, s=>5] = 1.0+0im
   return Op
 end
 
-function ITensors.op(::OpName"Ep2", ::SiteType"IdxSO6", s::Index)
+function ITensors.op(::OpName"Ep2", ::SiteType"IdxSU4_6d", s::Index)
   Op = emptyITensor(ComplexF64, s, s')
   Op[s'=>1, s=>4] = -1.0+0im
   Op[s'=>3, s=>6] = 1.0+0im
   return Op
 end
 
-function ITensors.op( ::OpName"Ep3", ::SiteType"IdxSO6", s::Index)
+function ITensors.op( ::OpName"Ep3", ::SiteType"IdxSU4_6d", s::Index)
   Op = emptyITensor(ComplexF64, s, s')
   Op[s'=>1, s=>2] = 1.0+0im
   Op[s'=>5, s=>6] = 1.0+0im
   return Op
 end
 
-function ITensors.op( ::OpName"Ep4", ::SiteType"IdxSO6", s::Index)
+function ITensors.op( ::OpName"Ep4", ::SiteType"IdxSU4_6d", s::Index)
   Op = emptyITensor(ComplexF64, s, s')
   Op[s'=>1, s=>5] = -1.0+0im
   Op[s'=>2, s=>6] = -1.0+0im
   return Op
 end
 
-function ITensors.op( ::OpName"Ep5", ::SiteType"IdxSO6", s::Index)
+function ITensors.op( ::OpName"Ep5", ::SiteType"IdxSU4_6d", s::Index)
   Op = emptyITensor(ComplexF64, s, s')
   Op[s'=>1, s=>3] = 1.0+0im
   Op[s'=>4, s=>6] = -1.0+0im
   return Op
 end
 
-function ITensors.op( ::OpName"Ep6", ::SiteType"IdxSO6", s::Index)
+function ITensors.op( ::OpName"Ep6", ::SiteType"IdxSU4_6d", s::Index)
   Op = emptyITensor(ComplexF64, s, s')
   Op[s'=>2, s=>3] = 1.0+0im
   Op[s'=>4, s=>5] = 1.0+0im
   return Op
 end
 
-function ITensors.op( ::OpName"Em1", ::SiteType"IdxSO6", s::Index)
+function ITensors.op( ::OpName"Em1", ::SiteType"IdxSU4_6d", s::Index)
   Op = emptyITensor(ComplexF64, s, s')
   Op[s'=>4, s=>2] = 1.0+0im
   Op[s'=>5, s=>3] = 1.0+0im
   return Op
 end
 
-function ITensors.op( ::OpName"Em2", ::SiteType"IdxSO6", s::Index)
+function ITensors.op( ::OpName"Em2", ::SiteType"IdxSU4_6d", s::Index)
   Op = emptyITensor(ComplexF64, s, s')
   Op[s'=>4, s=>1] = -1.0+0im
   Op[s'=>6, s=>3] = 1.0+0im
   return Op
 end
 
-function ITensors.op( ::OpName"Em3", ::SiteType"IdxSO6", s::Index)
+function ITensors.op( ::OpName"Em3", ::SiteType"IdxSU4_6d", s::Index)
   Op = emptyITensor(ComplexF64, s, s')
   Op[s'=>2, s=>1] = 1.0+0im
   Op[s'=>6, s=>5] = 1.0+0im
   return Op
 end
 
-function ITensors.op( ::OpName"Em4", ::SiteType"IdxSO6", s::Index)
+function ITensors.op( ::OpName"Em4", ::SiteType"IdxSU4_6d", s::Index)
   Op = emptyITensor(ComplexF64, s, s')
   Op[s'=>5, s=>1] = -1.0+0im
   Op[s'=>6, s=>2] = -1.0+0im
   return Op
 end
 
-function ITensors.op( ::OpName"Em5", ::SiteType"IdxSO6", s::Index)
+function ITensors.op( ::OpName"Em5", ::SiteType"IdxSU4_6d", s::Index)
   Op = emptyITensor(ComplexF64, s, s')
   Op[s'=>3, s=>1] = 1.0+0im
   Op[s'=>6, s=>4] = -1.0+0im
   return Op
 end
 
-function ITensors.op(::OpName"Em6", ::SiteType"IdxSO6", s::Index)
+function ITensors.op(::OpName"Em6", ::SiteType"IdxSU4_6d", s::Index)
   Op = emptyITensor(ComplexF64, s, s')
   Op[s'=>3, s=>2] = 1.0+0im
   Op[s'=>5, s=>4] = 1.0+0im
@@ -124,37 +130,68 @@ function ITensors.op(::OpName"Em6", ::SiteType"IdxSO6", s::Index)
 end
 
 # Define the sites
-sites = siteinds("IdxSO6", N)
+sites = siteinds("IdxSU4_6d", N)
+
+function pos_to_idx(cell_pos,sublat_idx,Lx,Ly,n_in_cell)
+  # Convert the cell position and sublattice index to a linear index
+  pbc_pos = [mod1(cell_pos[1],Lx),mod1(cell_pos[2],Ly)]
+  return (pbc_pos[1]-1)*Ly*n_in_cell+(pbc_pos[2]-1)*n_in_cell+sublat_idx
+end
+
+# Define the triangular lattice in open boundary condition
+Lattice_triangle = Vector{}()
+for x in 1:Lx, y in 1:Ly, sublat_idx in 1:n_in_cell
+    i  = pos_to_idx([x,y], sublat_idx, Lx, Ly, n_in_cell)
+
+    ## right 
+    if x < Lx
+      j  = pos_to_idx([x+1,y], sublat_idx, Lx, Ly, n_in_cell)
+      push!(Lattice_triangle, (i,j))
+    end
+
+    ## up
+    j  = pos_to_idx([x,y+1], sublat_idx, Lx, Ly, n_in_cell)
+    push!(Lattice_triangle, (i,j))
+
+    ## up left
+    if x >1 
+        j  = pos_to_idx([x-1,y+1], sublat_idx, Lx, Ly, n_in_cell)
+        push!(Lattice_triangle, (i,j))
+    end
+end
+
+
 
 #  AutoMPO 
-function build_h_1d_chain(sites; J1=1.0, J2=1.0, J3=1.0, J4=1.0, J5=1.0, J6=1.0, J7=1.0, J8=1.0, J9=1.0)
-    N = length(sites)  # 系统大小
+function build_h_2d(sites; Lattice, Lx,Ly, J1=1.0, J2=1.0, J3=1.0, J4=1.0, J5=1.0, J6=1.0, J7=1.0, J8=1.0, J9=1.0)
+    N = length(sites)  # system size
+    @assert N == Lx*Ly
+
     ampo = AutoMPO()
-    for i in 1:N-1  
+    for (i,j) in Lattice  
         # H
-        ampo += J1, "H1", i, "H1", i+1
-        ampo += J1, "H2", i, "H2", i+1
-        ampo += J1, "H3", i, "H3", i+1
+        ampo += J1, "H1", i, "H1", j
+        ampo += J2, "H2", i, "H2", j
+        ampo += J3, "H3", i, "H3", j
         # E
-        ampo += 0.5*J4, "Ep1", i, "Em1", i+1
-        ampo += 0.5*J4, "Em1", i, "Ep1", i+1
-        ampo += 0.5*J5, "Ep2", i, "Em2", i+1
-        ampo += 0.5*J5, "Em2", i, "Ep2", i+1
-        ampo += 0.5*J6, "Ep3", i, "Em3", i+1
-        ampo += 0.5*J6, "Em3", i, "Ep3", i+1
-        ampo += 0.5*J7, "Ep4", i, "Em4", i+1
-        ampo += 0.5*J7, "Em4", i, "Ep4", i+1
-        ampo += 0.5*J8, "Ep5", i, "Em5", i+1
-        ampo += 0.5*J8, "Em5", i, "Ep5", i+1
-        ampo += 0.5*J9, "Ep6", i, "Em6", i+1
-        ampo += 0.5*J9, "Em6", i, "Ep6", i+1
+        ampo += 0.5*J4, "Ep1", i, "Em1", j
+        ampo += 0.5*J4, "Em1", i, "Ep1", j
+        ampo += 0.5*J5, "Ep2", i, "Em2", j
+        ampo += 0.5*J5, "Em2", i, "Ep2", j
+        ampo += 0.5*J6, "Ep3", i, "Em3", j
+        ampo += 0.5*J6, "Em3", i, "Ep3", j
+        ampo += 0.5*J7, "Ep4", i, "Em4", j
+        ampo += 0.5*J7, "Em4", i, "Ep4", j
+        ampo += 0.5*J8, "Ep5", i, "Em5", j
+        ampo += 0.5*J8, "Em5", i, "Ep5", j
+        ampo += 0.5*J9, "Ep6", i, "Em6", j
+        ampo += 0.5*J9, "Em6", i, "Ep6", j
     end
     return MPO(ampo, sites)
 end
 
 # Hamiltonian
-H = build_h_1d_chain(sites; J1=1.0, J2=1.0, J3=1.0, J4=1.0, J5=1.0, J6=1.0, J7=1.0, J8=1.0, J9=1.0)
-
+H = build_h_2d(sites; Lattice = Lattice_triangle , Lx=Lx, Ly=Ly, J1=1.0, J2=1.0, J3=1.0, J4=1.0, J5=1.0, J6=1.0, J7=1.0, J8=1.0, J9=1.0)
 
 # DMRG 
 psi0 = randomMPS(sites)
